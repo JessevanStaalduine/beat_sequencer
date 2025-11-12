b59254a4858ce8f6478924767830f46d1fee4555
Jesse van Staalduine <staalduinejesse@gmail.com>
2025-11-12 10:06:50 +0100

    Fix Vite paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,13 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: './', // <--- gebruik relatieve paden in plaats van absolute
+  base: '/beat_sequencer/', // dit is je repo-naam
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
 });
