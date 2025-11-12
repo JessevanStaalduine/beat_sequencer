e864f6cfafc1f8582db2be70abab9abcb4563319
Jesse van Staalduine <staalduinejesse@gmail.com>
2025-11-12 10:13:35 +0100

    Fix back to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/beat_sequencer/', // dit is je repo-naam
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-      },
-    },
-  },
+  base: './', // <--- gebruik relatieve paden in plaats van absolute
 });
